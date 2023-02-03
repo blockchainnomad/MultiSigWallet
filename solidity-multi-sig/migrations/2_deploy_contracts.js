494ede9602f04ef1609853830ae7b118615caab8
dev-jacobs <40536266+blockchainnomad@users.noreply.github.com>
2023-02-03 18:21:22 +0900

    address update

--- solidity-multi-sig/migrations/2_deploy_contracts.js
+++ solidity-multi-sig/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const MultiSigWallet = artifacts.require("MultiSigWallet");
 
 module.exports = function (deployer) {
-  deployer.deploy(MultiSigWallet, ["account1", "account2", "account3"], 2);
+  deployer.deploy(MultiSigWallet, ["0xC587A2A2b010478F261a630bD21e10512439A226", "0xA1DeFD82D7482B261458923C4Fc0D4573e6aF244", "0x7a9AdC4f22aca6a9e505C7207738A0AA46d5b4b1"], 2);
 };
